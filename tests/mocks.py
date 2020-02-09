c5ab10a96fc78c761c8c642f316c68acfb8a6892
paulamh <paulamaki.henri@gmail.com>
2020-02-09 13:49:28 +0200

    add black + start writing menu tests

---
+++ tests/mocks.py
@@ -0,0 +1,7 @@
+class IdleUI:
+    def __init__(self): pass
+    def show_menu(self): pass
+    def get_menu_selection(self): pass
+    def show_options(self): pass
+    def get_options(self): pass
+    def show_history(self): pass
---
+++ tests/test_menu.py
@@ -0,0 +1,18 @@
+from pesis.menu.menu import Menu
+from test.mocks import IdleUI
+
+# test data / mocks
+
+
+# tests
+
+def test_something():
+    """
+    - has a bunch of options to be changed
+    - has launch game trigger
+    - can launch a Game(ui, options),
+    which gives control back to menu only when it exits
+    - has history that shows what previous Game-launches have done
+    """
+
+    Menu(ui)
